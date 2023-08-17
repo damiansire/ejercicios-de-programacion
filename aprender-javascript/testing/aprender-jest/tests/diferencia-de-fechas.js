8a73f61b0dcc648f2df877be22f9017c7f71ad42
Damian Sire <damiansire@hotmail.com>
2023-08-17 04:31:39 -0300

    Ahora permito devolver el tiempo faltante completo

--- aprender-javascript/testing/aprender-jest/tests/diferencia-de-fechas.js
+++ aprender-javascript/testing/aprender-jest/tests/diferencia-de-fechas.js
@@ -53,20 +53,46 @@ const timeUnits = {
 };
 
 /**
+ * Calcula el tiempo restante en todas las unidades de tiempo hasta la fecha objetivo.
+ * @param {number} targetDate - La fecha objetivo en milisegundos desde el 1 de enero de 1970.
+ * @returns {string} Cadena formateada con el tiempo restante en diferentes unidades.
+ */
+const formatRemainingTime = (remainingTime) => {
+    const days = Math.floor(remainingTime / timeUnits.days);
+    const hours = Math.floor((remainingTime % timeUnits.days) / timeUnits.hours);
+    const minutes = Math.floor((remainingTime % timeUnits.hours) / timeUnits.minutes);
+    const seconds = Math.floor((remainingTime % timeUnits.minutes) / timeUnits.seconds);
+
+    const parts = [];
+    if (days > 0) parts.push(`${days} día${days > 1 ? 's' : ''}`);
+    if (hours > 0) parts.push(`${hours} hora${hours > 1 ? 's' : ''}`);
+    if (minutes > 0) parts.push(`${minutes} minuto${minutes > 1 ? 's' : ''}`);
+    if (seconds > 0) parts.push(`${seconds} segundo${seconds > 1 ? 's' : ''}`);
+
+    return `Faltan ${parts.join(' con ')}`;
+};
+
+/**
  * Calcula el tiempo restante en la unidad de tiempo especificada hasta la fecha objetivo.
  * @param {number} targetDate - La fecha objetivo en milisegundos desde el 1 de enero de 1970.
- * @param {string} unit - La unidad de tiempo en la que se desea obtener el resultado (por defecto: 'days').
- *Valores posibles: 'days', 'hours', 'minutes', 'seconds'.
- * @returns {number} El tiempo restante en la unidad de tiempo especificada.
+ * @param {string} unit - La unidad de tiempo en la que se desea obtener el resultado (opcional).
+ *                        Valores posibles: 'days', 'hours', 'minutes', 'seconds'.
+ * @returns {string} Cadena formateada con el tiempo restante en la unidad de tiempo especificada.
  * @throws {Error} Si se especifica una unidad de tiempo no válida.
  */
-const remainingTimeToDate = (targetDate, unit = 'days') => {
+const remainingTimeToDate = (targetDate, unit) => {
     const currentTime = Date.now();
     const remainingMilliseconds = Math.max(targetDate - currentTime, 0);
+
+    if (!unit) {
+        return formatRemainingTime(remainingMilliseconds);
+    }
+
     const conversionFactor = timeUnits[unit];
 
     if (conversionFactor) {
-        return remainingMilliseconds / conversionFactor;
+        const remainingTime = Math.floor(remainingMilliseconds / conversionFactor);
+        return formatRemainingTime(remainingTime);
     } else {
         throw new Error('Invalid time unit specified');
     }
