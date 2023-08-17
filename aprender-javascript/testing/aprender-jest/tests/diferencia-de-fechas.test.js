35e460bfa3e6bb0fbb9c3dd0c4d1fd9d33412323
Damian Sire <damiansire@hotmail.com>
2023-08-17 04:44:08 -0300

    Agrego test para la diferencia en segundos

--- aprender-javascript/testing/aprender-jest/tests/diferencia-de-fechas.test.js
+++ aprender-javascript/testing/aprender-jest/tests/diferencia-de-fechas.test.js
@@ -7,3 +7,34 @@ describe("remainingDaysToDate", () => {
         expect(result).toBe(0)
     })
 })
+
+/*
+describe("differenceInSeconds function", () => {
+    it("should return 2 when there are 2 seconds difference between the dates", () => {
+        const startDate = new Date(2020, 2, 2, 16, 28, 10);
+        const endDate = new Date(2020, 2, 2, 16, 28, 12);
+
+        const result = differenceInSeconds(startDate, endDate);
+
+        expect(result).toBe(2);
+    });
+
+    it("should handle negative time differences", () => {
+        const startDate = new Date(2020, 2, 2, 16, 28, 12);
+        const endDate = new Date(2020, 2, 2, 16, 28, 10);
+
+        const result = differenceInSeconds(startDate, endDate);
+
+        expect(result).toBe(-2);
+    });
+
+    it("should handle same dates", () => {
+        const date1 = new Date(2020, 2, 2, 16, 28, 10);
+        const date2 = new Date(2020, 2, 2, 16, 28, 10);
+
+        const result = differenceInSeconds(date1, date2);
+
+        expect(result).toBe(0);
+    });
+});
+*/
